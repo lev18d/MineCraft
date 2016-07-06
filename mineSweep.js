76344bcec541e3426313fbe80cab4c70cce74470
sammy <sammy.diamantstein@mail.utoronto.ca>
2016-07-06 16:38:54 +0300

    adding images

--- mineSweep.js
+++ mineSweep.js
@@ -2,3 +2,14 @@
  * Created by itc_user on 7/5/2016.
  */
 
+var mineCraft = {};
+
+mineCraft.init = function() {
+
+
+    mineCraft.matrix = [["", "", "", "", ""]];
+    console.log(mineCraft.matrix.length);
+
+
+
+}
